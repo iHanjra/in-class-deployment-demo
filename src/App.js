5c14794d8f527764f2be7282e9a2142b2a0c817d
Irfan Hanjra <ihanjra@gmail.com>
2023-04-27 12:18:42 -0400

    Added env

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  console.log(process.env)
-  return <div className="App">Simple React App</div>;
+  console.log(process.env);
+
+  return (
+    <div className="App">
+      Simple React App
+      <div>Current Env: {process.env.NODE_ENV}</div>
+      <div>
+        Current API{" "}
+        {process.env.NODE_ENV === "development"
+          ? process.env.REACT_APP_DEVELOPMENT_API_KEY
+          : process.env.REACT_APP_PRODUCTION}
+      </div>
+    </div>
+  );
 }
 
 export default App;
